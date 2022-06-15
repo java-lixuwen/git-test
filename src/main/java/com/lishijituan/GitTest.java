9302773507ea67bdb9ff74c76ef3799c69b511ec
lixuwen <lixuwen2017@163.com>
2022-06-15 23:36:57 +0800

    idea 第一次提交

---
+++ src/main/java/com/lishijituan/GitTest.java
@@ -0,0 +1,14 @@
+package com.lishijituan;
+
+/**
+ * @Progrom: git-test
+ * @ClassName: GitTest
+ * @Description: TODO
+ * @Outhor: lixuwen
+ * @Create: 2022-06-15 23:33
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
